3616ff0582c047f2d0910c89b42c548c8c18d523
Chitmin <aungchitmin135@gmail.com>
2025-03-20 19:33:03 +0700

    add sidebar layout

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,96 @@
+import * as React from "react";
+import {
+  Frame,
+  House,
+  ListMusic,
+  ListVideo,
+  Map,
+  PieChart,
+  TrendingUp,
+  User,
+} from "lucide-react";
+
+import { NavMain } from "@/components/nav-main";
+import { NavProjects } from "@/components/nav-projects";
+import { NavUser } from "@/components/nav-user";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+const data = {
+  user: {
+    name: "Joshua",
+    email: "m@example.com",
+    avatar: "https://github.com/shadcn.png",
+  },
+  navMain: [
+    {
+      title: "Home",
+      url: "#",
+      icon: House,
+      isActive: true,
+    },
+    {
+      title: "Songs",
+      url: "#",
+      icon: ListVideo,
+    },
+    {
+      title: "Playlist",
+      url: "#",
+      icon: ListMusic,
+    },
+    {
+      title: "Just For You",
+      url: "#",
+      icon: User,
+    },
+    {
+      title: "Top Charts",
+      url: "#",
+      icon: TrendingUp,
+    },
+  ],
+  projects: [
+    {
+      name: "Design Engineering",
+      url: "#",
+      icon: Frame,
+    },
+    {
+      name: "Sales & Marketing",
+      url: "#",
+      icon: PieChart,
+    },
+    {
+      name: "Travel",
+      url: "#",
+      icon: Map,
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" variant="inset" {...props}>
+      <SidebarHeader className="mb-8">
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <NavUser user={data.user} />
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        <NavProjects projects={data.projects} />
+      </SidebarContent>
+    </Sidebar>
+  );
+}
