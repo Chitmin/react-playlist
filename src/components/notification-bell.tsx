59b9ad7d4189426bd0935100d027e2423033a76d
Chitmin <aungchitmin135@gmail.com>
2025-03-21 08:29:37 +0700

    minor css

--- src/components/notification-bell.tsx
+++ src/components/notification-bell.tsx
@@ -2,7 +2,7 @@ import { Bell } from "lucide-react";
 
 export function NotificationBell({ count = 0 }: { count?: number }) {
   return (
-    <div className="relative inline-block">
+    <div className="relative inline-block cursor-pointer">
       <Bell />
       {count > 0 && (
         <div className="absolute top-[-4px] bottom-[auto] right-[-10px] left-[auto] bg-destructive text-white rounded-[40%] px-2 py-1 w-6 h-4 whitespace-nowrap text-[10px] font-light leading-[0.7] inline-block text-center">
